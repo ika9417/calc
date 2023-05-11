c464f831c808a1e29d19a76023a99f0836865f90
Brave_Robin <sergeand@gmx.de>
2023-05-11 21:53:50 +0400

    Added math module

---
+++ 01_step.py
@@ -0,0 +1,22 @@
+import math
+
+
+'''
+ Please read https://docs.python.org/3/library/math.html
+'''
+
+# print(0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1)
+# print(sum([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]))
+
+edenitca = math.fsum([0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
+
+print(edenitca)
+
+# import os
+# import datetime
+# import requests
+
+# print("\n\n\n\n\n", os.getcwd())
+# print(datetime.datetime.now())
+
+# print(requests.get("https://docs.python.org/3/library/index.html").text)
