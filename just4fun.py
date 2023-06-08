71007fe26594f881954171166df9fe99fe41629f
Brave_Robin <sergeand@gmx.de>
2023-06-08 21:13:45 +0400

    The solution

--- just4fun.py
+++ just4fun.py
@@ -45,22 +45,35 @@ MORSE = {
 
 
 def morse_decoder(code: str) -> str:
+    wc = 0
     temporary_date = ""
     for coded_world in code.split("   "):
+        lc = 0
         for coded_letter in coded_world.split(" "):
-            temporary_date = temporary_date + MORSE[coded_letter]
+            if (wc == 0 and lc == 0):
+                temporary_date = temporary_date + MORSE[coded_letter].upper()
+            else:
+                temporary_date = temporary_date + MORSE[coded_letter]
+            lc += 1
         temporary_date = temporary_date+" "
-    return temporary_date
+        wc += 1
+
+    return "".join(temporary_date.strip())
 
 
 print("Example:")
 print(morse_decoder("... --- -- .   - . -..- -"))
 print( morse_decoder("..   .-- .- ...   -... --- .-. -.   .. -.   .---- ----. ----. -----"))
-
-assert morse_decoder("... --- -- .   - . -..- -") == "some text"
+print(morse_decoder('...- ...-- .-. -.--   .---- ----- -. --.   ... - .-. .---- -. --.   .-- .---- - ....   ... ----- -- ...--   -. ..- -- -... ...-- .-. .....'))
+assert morse_decoder("... --- -- .   - . -..- -") == "Some text"
 assert (
     morse_decoder("..   .-- .- ...   -... --- .-. -.   .. -.   .---- ----. ----. -----")
-    == "i was born in 1990"
+    == "I was born in 1990"
 )
+assert morse_decoder('... --- -- .   - . -..- -') == 'Some text'
+assert morse_decoder('..   .-- .- ...   -... --- .-. -.   .. -.   .---- ----. ----. -----') == 'I was born in 1990'
+assert morse_decoder('.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --..') == 'Abcdefghijklmnopqrstuvwxyz'
+assert morse_decoder('----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----.   .- .-. .   -.. .. --. .. - ...') == '0123456789 are digits'
+assert morse_decoder('...- ...-- .-. -.--   .---- ----- -. --.   ... - .-. .---- -. --.   .-- .---- - ....   ... ----- -- ...--   -. ..- -- -... ...-- .-. .....') == 'V3ry 10ng str1ng w1th s0m3 numb3r5'
 
 print("The mission is done! Click 'Check Solution' to earn rewards!")
